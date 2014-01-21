ede93b8216c355824ff90e281f67bf0181840313
Doug Latornell <djl@douglatornell.ca>
2014-01-21 15:22:44 -0800

    Add arrow package as a project dependency.

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ except IOError:
     long_description = ''
 install_requires = [
     # see requirements.txt for versions most recently used in development
+    'arrow',
     'cliff',
     'requests',
 ]

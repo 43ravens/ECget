02f9f07ff114e903e0f6166a89847ebccf7aec96
Doug Latornell <djl@douglatornell.ca>
2014-01-21 17:03:08 -0800

    Add methods to process raw data from EC river site to daily avg flows.

--- ecget/river.py
+++ ecget/river.py
@@ -20,6 +20,7 @@ import logging
 import time
 
 import arrow
+import bs4
 import cliff.command
 import requests
 import stevedore.driver
@@ -31,21 +32,73 @@ class RiverFlow(cliff.command.Command):
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
+
+        parsed_args.station_id = '08MF005'
+        parsed_args.start_date = arrow.get(2014, 1, 1)
+        parsed_args.end_date = arrow.get(2014, 1, 1)
+
+        raw_data = self._get_data(
+            parsed_args.station_id,
+            parsed_args.start_date,
+            parsed_args.end_date,
+        )
+        daily_avgs = self._process_data(raw_data, parsed_args.end_date)
+
+    def _get_data(self, station_id, start_date, end_date):
         read_mgr = stevedore.driver.DriverManager(
             namespace='ecget.get_data',
             name='river.discharge',
             invoke_on_load=True,
         )
-        station_id = '08MF005'
-        start_date = arrow.get(2014, 1, 1)
-        end_date = arrow.get(2014, 1, 1)
-        read_mgr.driver.get_data(
-            station_id, start_date, end_date)
+        raw_data = read_mgr.driver.get_data(
+            station_id,
+            start_date,
+            end_date,
+        )
         msg = ('got {} river discharge data for {}'
-               .format(station_id, start_date.format('YYYY-MM-DD')))
+               .format(station_id,
+                       start_date.format('YYYY-MM-DD')))
         if start_date != end_date:
             msg += ' to {}'.format(end_date.format('YYYY-MM-DD'))
         self.log.debug(msg)
+        return raw_data
+
+    def _process_data(self, raw_data, end_date):
+        tds = raw_data.findAll('td')
+        timestamps = (td.string for td in tds[::2])
+        flows = (td.text for td in tds[1::2])
+        data_day = self._read_datestamp(tds[0].string)
+        flow_sum = count = 0
+        daily_avgs = []
+        for timestamp, flow in zip(timestamps, flows):
+            datestamp = self._read_datestamp(timestamp)
+            if datestamp > end_date.date():
+                break
+            if datestamp == data_day:
+                flow_sum += self._convert_flow(flow)
+                count += 1
+            else:
+                daily_avgs.append((data_day, flow_sum / count))
+                data_day = datestamp
+                flow_sum = self._convert_flow(flow)
+                count = 1
+        daily_avgs.append((data_day, flow_sum / count))
+        return daily_avgs
+
+    def _read_datestamp(self, string):
+        return arrow.get(string, 'YYYY-MM-DD HH:mm:ss').date()
+
+    def _convert_flow(self, flow_string):
+        """Convert a flow data value from a string to a float.
+
+        Handles 'provisional values' which are marked with a `*` at
+        the end of the string.
+        """
+        try:
+            return float(flow_string)
+        except ValueError:
+            # Ignore training `*`
+            return float(flow_string[:-1])
 
 
 class RiverDataBase(object):
@@ -81,7 +134,8 @@ class RiverDataBase(object):
         :arg end_date: Last date to get data for.
         :type end_date: :py:class:`arrow.Arrow` instance
 
-        :returns:
+        :returns: BeautifulSoup parser object containing data table
+                  from EC page.
         """
 
 
@@ -108,9 +162,10 @@ class RiverDischarge(RiverDataBase):
         :arg end_date: Last date to get data for.
         :type end_date: :py:class:`arrow.Arrow` instance
 
-        :returns:
+        :returns: BeautifulSoup parser object containing data table
+                  from EC page.
         """
-        last_date = end_date.replace(days=1)
+        last_date = end_date.replace(days=+1)
         self.params.update({
             'stn': station_id,
             'syr': start_date.year,
@@ -124,4 +179,5 @@ class RiverDischarge(RiverDataBase):
             s.post(self.DISCLAIMER_URL, data=self.DISCLAIMER_ACTION)
             time.sleep(2)
             response = s.get(self.DATA_URL, params=self.params)
-        self.log.info(response.text)
+        soup = bs4.BeautifulSoup(response.content)
+        return soup.find('table', id='dataTable')
